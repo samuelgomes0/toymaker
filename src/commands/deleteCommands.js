8ee3ca024a9c89c4eb77b02652317f09a48ac131
Samuel Gomes Rosa <samuel_gomes26@hotmail.com>
2024-01-10 02:12:24 -0300

    ✨ feat(commands): adiciona arquivos deleteCommands.js e deployCommands.js
    🔧 refactor(commands): implementa a lógica para atualizar e excluir comandos da aplicação no Discord

--- src/commands/deleteCommands.js
+++ src/commands/deleteCommands.js
@@ -1,5 +1,5 @@
 const { REST, Routes } = require("discord.js");
-const { clientId, token } = require("./config.json");
+const { clientId, token } = require("../config.json");
 
 const rest = new REST().setToken(token);
 
